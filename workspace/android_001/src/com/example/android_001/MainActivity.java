88ba21d8f75dd9341b26b0f9d8a86b109e382354
tonghuajianghan <tonghuajianghan@gmail.com>
2016-01-06 22:56:49 +0800

    style1

--- workspace/android_001/src/com/example/android_001/MainActivity.java
+++ workspace/android_001/src/com/example/android_001/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //加载activity_main.xml文件
         setContentView(R.layout.activity_main);
     }
 
