46a38643ac2dd036f1d1d33afa79292f4cefc7c5
tonghuajianghan <tonghuajianghan@gmail.com>
2016-01-06 22:21:56 +0800

    android-001

--- android_study.md
+++
@@ -1,5 +0,0 @@
-#android study note  
----
-搭建环境 
-
-1. eclipse + adt + sdk
---
+++ workspace/android_001/src/com/example/android_001/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.android_001;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public void test(View view){
+    	//获取文本框，并改变其中的内容，R类可以监控到所有的资源
+    	TextView textView = (TextView)findViewById(R.id.textView1);
+    	textView.setText("ccc");
+    	//类中引用资源文件中的内容
+    	textView.setText(R.string.msg);
+    	
+    	//输出
+    	Toast.makeText(MainActivity.this, "点击了button", Toast.LENGTH_SHORT).show();
+    }
+}
